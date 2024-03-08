b6c67c384100700fe769bf7d16cce5b4c20fb7cc
Juliano Sill <sill.juliano@gmail.com>
2024-03-07 23:15:34 -0300

    feat(header): create component

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from '@/components/header'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
+    <div className="mx-auto grid min-h-screen w-full max-w-[1440px] grid-rows-app gap-5 p-8">
       <Header />
       {children}
     </div>
